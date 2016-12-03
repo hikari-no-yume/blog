c8f5dc3db1971b4e860a650c6cbef6069239c2e4
iroegbu <iroegbuogechi@gmail.com>
2016-12-03 19:56:56 +0100

    Added disqus integrations

---
+++ src/script.js
@@ -0,0 +1,7 @@
+var config = {
+    'disqus': false
+};
+
+var disqus = {
+    'forum_name':       "//EXAMPLE.disqus.com/embed.js" // IMPORTANT: Replace EXAMPLE with your forum shortname!
+};
--- templates/post.php
+++ templates/post.php
@@ -1,10 +1,34 @@
 <!doctype html>
-<meta charset=utf-8>
-<title><?=htmlspecialchars($postTitle)?></title>
-<link rel=stylesheet href=style.css>
+<html>
+    <head>
+        <meta charset=utf-8>
+        <title><?=htmlspecialchars($postTitle)?></title>
+        <link rel=stylesheet href=style.css>
+    </head>
+    <body>
+        <main>
+            <a href=/>← Home</a><br>
 
-<main>
-     <a href=/>← Home</a><br>
+            <?=$postContent?>
+        </main>
+        <script src="script.js"></script>
 
-     <?=$postContent?>
-</main>
+        <script type="application/javascript">
+            (function() {
+                if (config.disqus) {
+                    _parent = document.getElementById("main");
+                    _disqus_div = document.createElement("div");
+                    _disqus_div.id = "disqus_thread";
+                    _parent.appendChild(_disqus_div);
+
+                    var d = document, s = d.createElement('script');
+                        
+                    s.src = disqus.forum_name;
+                    
+                    s.setAttribute('data-timestamp', +new Date());
+                    (d.head || d.body).appendChild(s);
+                }                    
+            })();
+        </script>
+    </body>
+</html>
